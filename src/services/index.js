9551d3f6ed2d17767a7f375fe3cd7c28ce12b1d6
Martin Autumn <martin@herbst-winter.net>
2016-04-06 13:12:35 +0200

    Temporarily disable authentification

--- src/services/index.js
+++ src/services/index.js
@@ -4,10 +4,10 @@ const user = require('./user');
 const mongoose = require('mongoose');
 module.exports = function() {
   const app = this;
-  
+
   mongoose.connect(app.get('mongodb'));
   mongoose.Promise = global.Promise;
-  
+
   app.configure(authentication);
   app.configure(user);
 };
